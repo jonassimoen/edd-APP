bf968760cdb9989de887faffa1a29b279e28583e
Jonas Simoen <jonas.simoen@ugent.be>
2023-10-22 02:05:20 +0200

    prevent sourcemap generation

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import reactRefresh from "@vitejs/plugin-react-refresh";
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-   sourcemap: true,
+   sourcemap: false,
    outDir: "build"
   },
   optimizeDeps: {
