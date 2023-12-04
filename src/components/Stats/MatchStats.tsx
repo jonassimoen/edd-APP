ce260c028ac2d3387e1f5b01ccba99f099b40bd5
Jonas Simoen <jonas.simoen@ugent.be>
2023-12-04 15:17:55 +0100

    fix(matchStats): remove unused props

--- src/components/Stats/MatchStats.tsx
+++ src/components/Stats/MatchStats.tsx
@@ -13,30 +13,10 @@ declare type MatchStatsProps = {
 export const MatchStats = (props: MatchStatsProps) => {
 	const { data } = useGetMatchQuery(props.matchId);
 	return (
-	// <StatsStyle>
-	//     <Row className="stat-row">
-	//         <Col lg={11} md={11} sm={11} xs={11}>
-	//         {/* <Col> */}
-	//             <ClubDetails left>
-	//                 <ClubName className="team-name" fullName={data?.home.name} short={data?.home.short} />
-	//                 <ClubBadgeBg src={`${config.API_URL}/static/badges/${data?.home.externalId}.png`} />
-	//             </ClubDetails>
-	//         </Col>
-	//         <Col lg={2} md={2} sm={2} xs={2}>
-	//             <b className="score started">{ `${props.homeScore} - ${props.awayScore}` }</b>
-	//         </Col>
-	//         <Col lg={11} md={11} sm={11} xs={11}>
-	//             <ClubDetails>
-	//                 <ClubBadgeBg src={`${config.API_URL}/static/badges/${data?.away.externalId}.png`} />
-	//                 <ClubName className="team-name" fullName={data?.away.name} short={data?.away.short} />
-	//             </ClubDetails>
-	//         </Col>
-	//     </Row>
-	// </StatsStyle>
 		<MatchStatsStyle>
-			<ClubDetails className="team" left>
-				<ClubName className="team-name" fullName={data?.home.name} short={data?.home.short} />
+			<ClubDetails className="team">
 				<ClubBadgeBg src={`${props.assetsCdn}/badges/${data?.home.id}.png`} />
+				<ClubName className="team-name" fullName={data?.home.name} short={data?.home.short} />
 			</ClubDetails>
 			<div className="score">
 				<b>{`${props.homeScore} - ${props.awayScore}`}</b>
